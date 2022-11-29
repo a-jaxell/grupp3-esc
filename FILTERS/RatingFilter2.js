c80440d0354627c98d4f4106dd6c2466a2cc5491
Dan Saa <dansaa8@protonmail.com>
2022-11-29 13:30:55 +0100

    ratingfilter2 reduced code with functions

--- FILTERS/RatingFilter2.js
+++ FILTERS/RatingFilter2.js
@@ -1,4 +1,8 @@
 export class RatingFilter2 {
+  minStars = [];
+  maxStars = [];
+
+
   constructor(listObj) {
     //passing in a reference to the classList object that calls this constructor
     this.minRating = 0;
@@ -15,86 +19,52 @@ export class RatingFilter2 {
   }
 
   render() {
+    const starRating = document.createElement("div"); //The outer container, will be returned from this function
+    starRating.className = "star-rating";
 
-    const starRating = document.createElement('div');
-    starRating.className = 'star-rating';
+    const amountOfStars = 5;
 
-    const inputA  = document.createElement('input');
-    inputA.setAttribute('type', 'radio');
-    inputA.setAttribute('name', 'stars');
-    inputA.setAttribute('id','star-a');
-    inputA.setAttribute('value','5');
-    const labelA = document.createElement('label');
-    labelA.setAttribute('for','star-a');
+    createLineOfStars(this.minStars, 'minStar', amountOfStars);
+    // createLineOfStars(this.maxStars, 'maxStar', amountOfStars);
 
-    const inputB = document.createElement('input');
-    inputB.setAttribute('type', 'radio');
-    inputB.setAttribute('name', 'stars');
-    inputB.setAttribute('id','star-b');
-    inputB.setAttribute('value','4');
-    const labelB = document.createElement('label');
-    labelB.setAttribute('for','star-b');
+    function createLineOfStars(arr, id, count) {
+      const lineOfStars = document.createElement("div");
+      lineOfStars.className = 'lineOfStars';
+      for (let i = count; i > 0; i--) {
 
-    const inputC = document.createElement('input');
-    inputC.setAttribute('type', 'radio');
-    inputC.setAttribute('name', 'stars');
-    inputC.setAttribute('id','star-c');
-    inputC.setAttribute('value','3');
-    const labelC = document.createElement('label');
-    labelC.setAttribute('for','star-c');
+        const input = document.createElement("input"); //this element is hidden with css
+        input.setAttribute("type", "radio");
+        input.setAttribute("name", "stars");
+        input.setAttribute("id", `${id}-${i}`);
+        input.setAttribute("value", [i]);
+        arr.push(input); //pushed here, so we can see which stars are checked
 
-    const inputD = document.createElement('input');
-    inputD.setAttribute('type', 'radio');
-    inputD.setAttribute('name', 'stars');
-    inputD.setAttribute('id','star-d');
-    inputD.setAttribute('value','2');
-    const labelD = document.createElement('label');
-    labelD.setAttribute('for','star-d');
+        const label = document.createElement("label"); //visual representation of the star
+        label.setAttribute("for", `${id}-${i}`);
 
-    const inputE = document.createElement('input');
-    inputE.setAttribute('type', 'radio');
-    inputE.setAttribute('name', 'stars');
-    inputE.setAttribute('id','star-e');
-    inputE.setAttribute('value','1');
-    const labelE = document.createElement('label');
-    labelE.setAttribute('for','star-e');
+        lineOfStars.append(input, label);
+      }
+      starRating.append(lineOfStars); //Appended to outer container
+    }
 
-    starRating.append(inputA, labelA);
-    starRating.append(inputB, labelB);
-    starRating.append(inputC, labelC);
-    starRating.append(inputD, labelD);
-    starRating.append(inputE, labelE);
 
-    const checkedStars = [inputE, inputD, inputC, inputB, inputA]; 
-    starRating.addEventListener('change', ()=> {
-        let highestVal = 0;
-        for (let i = 0 ; i < checkedStars.length; i++) {
-            if (checkedStars[i].checked && checkedStars[i].value >= highestVal){
-                highestVal = checkedStars[i].value;
-            }
+
+    starRating.addEventListener("change", () => { //EvenListener added to outer container.
+      let highestVal = 0;
+      for (let i = 0; i < amountOfStars; i++) {
+        if (
+          this.minStars[i].checked &&
+          this.minStars[i].value >= highestVal
+        ) {
+          highestVal = this.minStars[i].value;
         }
-        this.minRating = highestVal;
-        this.list.update();
+      }
+      this.minRating = highestVal;
+      this.list.update();
     });
-
-    
-
-    // render() {
-    //     const input = document.createElement('input');
-    //     input.type = 'range';
-    //     input.min = 0;
-    //     input.max = 5;
-    //     input.value = 0;
-    //     input.addEventListener('change', (ev) => {
-    //         this.minRating = ev.target.value;
-    //         this.list.update();
-    //     });
-    //     return input;
-    //   }
-    
-
 
     return starRating;
   }
+
   
 }
