c17f9eeb8cb6587da87a55d51ae3233404dd715c
a-jaxell <97404672+KvDread@users.noreply.github.com>
2022-11-30 19:39:06 +0100

    Add code to matching function and cleaned up code.

--- FILTERS/TypeFilter.js
+++ FILTERS/TypeFilter.js
@@ -1,32 +1,62 @@
 export class TypeFilter{
     constructor(list){
         this.list = list;
+        this.isOnlineChecked = null;
+        this.isOnsiteChecked = null;
     }
 
-    challengeMatch(challenge){
-        if(challenge.data.type){
+    // checkar ifall värdena för boxarna är sanna eller falska
+    // och returnerar värde därefter
+
+    challengeDoesMatch(challenge){
+        if(!this.isOnlineChecked && !this.isOnsiteChecked){
+            console.log('here');
+            return false;
+        }else if(challenge.data.type === 'online' && this.isOnlineChecked){
+            console.log('and here');
+            return true;
+        }else if(challenge.data.type === 'onsite' && this.isOnsiteChecked){
+            console.log('and here too');
             return true;
         }
     }
     
-    render(){
-        const inputOnline = document.createElement('input');
-        const inputOnsite = document.createElement('input');
+    // Skapar en container med 2 checkboxar i DOM och ger dem labels
+    // Båda får en eventlistener som updaterar respektive värde i objektet.
 
+    render(){
+        const checkBoxDiv = document.createElement('div');
+        checkBoxDiv.className = 'checkBoxSection';
+
+        const inputOnline = document.createElement('input');
+        inputOnline.id = 'checkBoxOnline';
         inputOnline.type = 'checkbox';
+
+        const labelOnline = document.createElement('label');
+        labelOnline.for = 'checkBoxOnline';
+        labelOnline.innerText = 'Include online challenges';
+        
+        const inputOnsite = document.createElement('input');
+        inputOnsite.id ='checkBoxOnsite'
         inputOnsite.type = 'checkbox';
 
-        inputOnline.isChecked = true;
-        inputOnsite.isChecked = true;
-
-
-        // .isChecked returnerar true/false om boxen är tickad
+        const labelOnsite = document.createElement('label');
+        labelOnsite.for = 'checkBoxOsite';
+        labelOnsite.innerText = 'Include onsite challenges';
+        
+        checkBoxDiv.append(inputOnline);
+        checkBoxDiv.append(labelOnline);
+        checkBoxDiv.append(inputOnsite);
+        checkBoxDiv.append(labelOnsite);
 
         inputOnline.addEventListener('change', (event) => {
-            return event.target.isChecked;
-                });
+            this.isOnlineChecked = event.target.checked;
+            this.list.update();
+        });
         inputOnsite.addEventListener('change', (event) => {
-            return event.target.isChecked;
-        })
+            this.isOnsiteChecked = event.target.checked;
+            this.list.update();
+        });
+        return checkBoxDiv;
     }
 }
