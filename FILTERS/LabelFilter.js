da3fbdbaac3b270c7db3edf5badd0a0e56e95884
a-jaxell <97404672+KvDread@users.noreply.github.com>
2022-11-30 12:55:15 +0100

    Add matching functionality

--- FILTERS/LabelFilter.js
+++ FILTERS/LabelFilter.js
@@ -2,29 +2,26 @@ export class LabelFilter{
     constructor(list){
         this.list = list;
         this.labelButtons = [];
-        // this.challenges = new ChallengeList;
+        this.matchArray = [];
     }
-    
-    // TODO: kolla hur man kan skriva om det med Array.filter();
-    // Ska skriva ut alla unikaLabels som finns i datan
+
+    // Matchar varje label i varje challenge emot en array som innehåller "nedtryckta" knappar
 
     challengeDoesMatch(challenge){
-
-        const labelButtons = document.querySelectorAll('.labelButton');
-        // labelButtons.addEventListener('click');
-
-        if(labelButtons.isChecked){
-
+        if(this.matchArray.some(ele => challenge.data.labels.includes(ele))){
+            return true;
+        } else {
+            return false;
         }
 
-        // Välja alla tagg-knappar
-        // Kolla ifall de är nedtryckta
-        // Visa de rum som matchar nedtryckta knappar
     }
     
+     // itererar genom den temporära Arrayen och skapar en 
+     // knapp med texten i array[i]
+     // Skapar en array som innehåller labels som ska matchas emot challenges
+     // TODO : lägga till en toggle av styling vid klick på knappar. 
+    
     render(){
-        console.log(this.list.challenges[3].data.labels);
-            // console.log(data.challenges);
         
              const labelSection = document.createElement('div');
              labelSection.className = 'labelSection';
@@ -39,30 +36,21 @@ export class LabelFilter{
             for(let i = 0; i < this.labelButtons.length; i++){
                 const labelButton = document.createElement('input');
                 labelButton.type = 'button';
-                labelButton.value = this.labelButtons[i];
                 labelSection.append(labelButton);
+                labelButton.value = this.labelButtons[i];
+                
             }
-            console.log(labelSection);
+            labelSection.addEventListener('click', (ev) => {
+                if(!this.matchArray.includes(ev.target.value)){
+
+                    this.matchArray.push(ev.target.value);
+                }else{
+
+                    this.matchArray = this.matchArray.filter(ele => ele !== ev.target.value);
+                }
+                this.list.update();
+            });
+
             return labelSection;
         }
     }     
-        // itererar genom den temporära Arrayen och skapar en 
-        // knapp med texten i array[i]
-        // Kanske går att förenkla med array metoder.
-        
-        // for(let i = 0; i < data.challenges.length; i++){
-        //     console.log('here');
-        //         data.challenges[i].labels.forEach( element => {
-        //             if(!tempData.includes(element)){
-        //                 tempData.push(element)
-        //             }
-        //             console.log(this.tempData);
-        //         })
-        // }
-        // for(let i = 0; i < data.challenges.length; i++){
-         
-         
-        // Här ska tagg-knappar renderas och få eventlisteners.
-        // Det ska finnas en uppsättning knappar som är unika. 
-        // är de nedtryckta ska de visas som grå.
-        // Vi kanske skulle kunna använda oss av radioknappar för detta. 
