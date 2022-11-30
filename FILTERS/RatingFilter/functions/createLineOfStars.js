fa1f782fe7963c503db8d0873797a9710524bc0d
Dan Saa <dansaa8@protonmail.com>
2022-11-30 20:47:37 +0100

    add event delegation, not working

--- FILTERS/RatingFilter/functions/createLineOfStars.js
+++ FILTERS/RatingFilter/functions/createLineOfStars.js
@@ -1,32 +1,34 @@
 export function createLineOfStars(ctr, arr, id, count) {
-      const lineOfStars = document.createElement("div");
-      lineOfStars.className = 'lineOfStars';
-      for (let i = count; i > 0; i--) {
 
-        const input = document.createElement("input"); //this element is hidden with css
-        input.setAttribute("type", "radio");
-        input.setAttribute("name", [id]);
-        input.setAttribute("id", `${id}-${i}`);
-        input.setAttribute("value", [i]);
-        arr.push(input); //pushed here, so we can see which stars are checked
+  const lineOfStars = document.createElement("div");
+  lineOfStars.className = "lineOfStars";
 
-        const label = document.createElement("label"); //visual representation of the star
-        label.setAttribute("for", `${id}-${i}`);
-        label.setAttribute("id", `${id}-${i}`);
+  for (let i = count; i > 0; i--) {
 
-        label.addEventListener('click', () => {
-          if (input.checked) {
-            const checkedVal = Number(input.value);
-            console.log(checkedVal);
-          }
-        })
+    const input = document.createElement("input"); //this element is hidden with css
+    input.setAttribute("type", "radio");
+    input.setAttribute("name", [id]);
+    input.setAttribute("id", `${id}-${i}`);
+    input.setAttribute("value", [i]);
+    arr.push(input, input.value); //pushed here, so we can see which stars are checked
 
-        lineOfStars.append(input, label);
-      }
+    const label = document.createElement("label"); //visual representation of the star
+    label.setAttribute("for", `${id}-${i}`);
+    label.setAttribute("id", `${id}-${i}`);
 
-      lineOfStars.addEventListener('change', () =>  {
-        const arr = document.querySelector('.lineOfStars');
-      })
-      ctr.append(lineOfStars); //Appended to outer container
-    }
+    lineOfStars.append(input, label);
+
+  }
+  
+  ctr.append(lineOfStars); //Appended to outer container
+
+
+  let selectedLabel;
+
+  lineOfStars.onclick = ('click', function(event) { //event delegation
+    let element = event.target;
+    if(element.checked)
+    console.log(`Clicked button: ${element}`);
+  });
+}
 
