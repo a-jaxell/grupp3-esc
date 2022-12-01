aca90bce08537b5894e43da88aadda47f4a85f9a
burrozd <95920902+burrozd@users.noreply.github.com>
2022-12-01 10:47:39 +0100

    booking update

--- booking.js
+++ booking.js
@@ -1,124 +1,110 @@
-const bookingContainer = document.querySelector(".booking-container");
+import { queryJson } from "./api.js";
 
-const bookingButton = document.createElement("button");
-bookingButton.className = "booking-button";
+queryJson();
 
-// first page
+const takeData = async() => {
+    console.log(await queryJson("2022-12-12", 6));
+    const dataArray = await queryJson(
+        document.getElementById("date-input").value
+    );
 
-let headerFirstPage = document.createElement("h1");
-headerOne.className = "title-firstPage";
+    /*             document.querySelector(".cardID") */
+    console.log(dataArray);
+
+    ["värde1", "värde2", "osv"];
+    for (let i = 0; i < dataArray.length; i++) {
+        const option = document.createElement("option");
+        option.innerText = array[i];
+        document.getElementById("time-input").appendChild(option);
+    }
+};
+
+const popOutButton = document.getElementById("bookingButton");
+
+const bg_container = document.querySelector(".booking-container");
+// Add event listeners
+let titleFirstPage = document.createElement("h1");
+titleFirstPage.className = "title-firstPage";
+titleFirstPage.innerText = "Step one of booking";
 
 let paragraphOne = document.createElement("p");
 paragraphOne.className = "paragraph-one";
+paragraphOne.innerText = "Date:";
 
 let dateInput = document.createElement("input");
-dateInput.setAttribute("date");
 dateInput.className = "date-input";
-
+dateInput.setAttribute("id", "date-input");
+dateInput.placeholder = "YYYY-MM-DD";
+/* dateInput.required = true;
+        dateInput.setAttribute("pattern", "d{4}-(?:d{2}-){2}"); */
 let dateButton = document.createElement("button");
-dateButton.className = "date-button";
+dateButton.className = "dating-button";
+dateButton.innerText = "Search avaliable times";
 
-// second page
+function returnText() {
+    let input = document.getElementById("userInput").value;
+    alert(input);
+}
 
-let titleSecondPage = document.createElement("h1");
-headerOne.className = "title-secondPage";
+popOutButton.addEventListener("click", popOutNow());
 
-let nameHeader = document.createElement("h3");
-nameHeader.className = "name-header";
-
-let nameInput = document.createElement("input");
-nameInput.setAttribute("text");
-nameInput.className = "name-input";
-
-let emailHeader = document.createElement("h3");
-emailHeader.className = "email-header";
-
-let emailInput = document.createElement("input");
-emailInput.setAttribute("email");
-emailInput.className = "email-input";
-
-let timeHeader = document.createElement("h3");
-timeHeader.className = "time-header";
-
-let timeInput = document.createElement("input");
-timeInput.setAttribute("time");
-timeInput.className = "time-input";
-
-let participantsHeader = document.createElement("h3");
-timeHeader.className = "participants-header";
-
-let participants = document.querySelector("input");
-participants.className = "participants-amount";
-
-let detailsButton = document.createElement("button");
-detailsButton.className = "details-button";
-
-//  Third page
-
-let titleThirdPage = document.createElement("h1");
-headerOne.className = "title-ThirdPage";
-
-let completedLink = document.createElement("a");
-completedLink.className = "back-to-start";
-/* 
-
-
- */
-bookingButton.addEventListener("click", bookingSystem());
-
-function bookingSystem() {
-    if (bookingButton.clicked == true && dateInput.value.length != 0) {
+function popOutNow() {
+    document.getElementById("bookingButton").onclick = function() {
         document.querySelector(".booking-container").style.display = "flex";
-        bookingContainer.appendChild(headerFirstPage);
-        bookingContainer.appendChild(paragraphOne);
-        bookingContainer.appendChild(dateInput);
-        bookingContainer.appendChild(dateButton);
-    }
-    return;
+        bg_container.appendChild(titleFirstPage);
+        bg_container.appendChild(paragraphOne);
+        bg_container.appendChild(dateInput);
+        //
+        bg_container.appendChild(dateButton);
 
-    function secondPage() {
-        if (dateButton.clicked == true) {
-            document.querySelector(".booking-container").style.display = "flex";
-            bookingContainer.element.remove(headerFirstPage);
-            bookingContainer.element.remove(paragraphOne);
-            bookingContainer.element.remove(dateInput);
-            bookingContainer.element.remove(dateButton);
-            //
-            bookingContainer.appendChild(titleSecondPage);
-            bookingContainer.appendChild(nameHeader);
-            bookingContainer.appendChild(nameInput);
-            bookingContainer.appendChild(emailHeader);
-            bookingContainer.appendChild(emailInput);
-            bookingContainer.appendChild(timeHeader);
-            bookingContainer.appendChild(timeInput);
-            bookingContainer.appendChild(participantsHeader);
-            bookingContainer.appendChild(participants);
+        /*  if (dateInput.match( ? pattern ? ) && dateButton.onclick()) {}return; */
+        dateButton.addEventListener("click", evalInput);
+    };
 
-            bookingContainer.appendChild(detailsButton);
+    async function evalInput() {
+        const regex = /^\d{4}-\d{2}-\d{2}$/; //right format
+        const date = new Date();
+        const validDate = addOneYear(date);
+        console.log("ValidDate: ", validDate);
+
+        const userInput = dateInput.value.trim();
+        const inputDate = new Date(userInput);
+
+        if (userInput.match(regex) === null) {
+            console.log("You entered wrong format");
+        } else if (inputDate > validDate) {
+            console.log("choose a date within the next year");
+        } else {
+            const availTimes = await queryJson(userInput); // !availTimes.length === 0)
+            okFormatAndTime(availTimes);
         }
-        return;
     }
 
-    function thirdPage() {
-        if (
-            detailsButton.clicked == true &&
-            nameInput.value.length != 0 &&
-            emailInput.value.length != 0 &&
-            timeInput.value.length != 0
-        ) {
-            document.querySelector(".booking-container").style.display = "flex";
-            bookingContainer.element.remove(titleSecondPage);
-            bookingContainer.element.remove(nameHeader);
-            bookingContainer.element.remove(nameInput);
-            bookingContainer.element.remove(emailHeader);
-            bookingContainer.element.remove(emailInput);
-            bookingContainer.element.remove(timeHeader);
-            bookingContainer.element.remove(timeInput);
-            bookingContainer.element.remove(detailsButton);
-            //
-            bookingContainer.appendChild(titleThirdPage);
-            bookingContainer.appendChild(completedLink);
-            return;
+    function okFormatAndTime(availTimes) {
+        if (availTimes.length === 0) {
+            //TypeError: undefined is not a function (near '...availTimes...')
+            //TypeError: undefined is not an object (evaluating 'availTimes.length')
+            console.log("No times found on that date");
+        } else {
+            console.log("These are the available dates: ", availTimes);
+            secondPage();
+
+            // thirdPage();
         }
+    }
+
+    function addOneYear(date) {
+        date.setFullYear(date.getFullYear() + 1); // 2022-12-12"
+
+        return date;
     }
 }
+
+/* Failed to load resource: the server responded with a status of 400()
+
+        {error: "Invalid date. Must be within next year. Use ?date=2021-05-01 format"}
+
+         */
+/* if (dateStr.match(regex) === null) {
+            return false;
+        } */
