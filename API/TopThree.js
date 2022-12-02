55d0c7288a2ed7223726cb14a90b0ced1f25ba71
a-jaxell <97404672+KvDread@users.noreply.github.com>
2022-12-02 12:22:22 +0100

    Add logic and render for top three cards

--- API/TopThree.js
+++ API/TopThree.js
@@ -6,14 +6,12 @@ export class TopThree{
         this.api = new APIAdapter();
     }    
     
-    topThree(challenge){
-        const challenges = this.api.loadChallenges();
-        challenges.sort((a, b))
+    async topThree(){
+        const challenges = await this.api.loadChallenges();
+        challenges.sort((a, b) => b.data.rating - a.data.rating);
+        for(let i = 0; i < 3; i++){
+            document.querySelector('.challenges').append(challenges[i].render());
+        } 
+    } 
     }
-
-    render(challenge) {
-        console.log(challenges);
-
-        this[challenge].data.rating
-    }
-}
+    
--- API/index.js
+++ API/index.js
@@ -5,14 +5,8 @@ import { TopThree } from "./TopThree.js";
     const challengeInsert = document.querySelector('.challenges');
     const challengesCTA = document.querySelector('.challenges-cta');
     const topThreeChallenges = new TopThree();
-    const addTopThree = topThreeChallenges.render();
-    console.log(challengeInsert);
-    // challengeInsert.insertBefore(addTopThree, challengesCTA);
-    console.log('here');
+    const addTopThree = topThreeChallenges.topThree();
 
 
-    const array = [[4], [5], [3], [2], [1.5], [0], [2.5]];
+
     
-    console.log(array.sort((a, b) => b - a));
-    challenge.data.rating[i]
-    challenge
