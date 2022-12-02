a0df5766dc1c546376250ae1d40a3a8be13fcf24
a-jaxell <97404672+KvDread@users.noreply.github.com>
2022-12-02 12:35:26 +0100

    Add correct querySelector target of card render

--- API/index.js
+++ API/index.js
@@ -1,12 +1,4 @@
 import { TopThree } from "./TopThree.js";
 
-
-    
-    const challengeInsert = document.querySelector('.challenges');
-    const challengesCTA = document.querySelector('.challenges-cta');
     const topThreeChallenges = new TopThree();
-    const addTopThree = topThreeChallenges.topThree();
-
-
-
-    
+    topThreeChallenges.topThree();
