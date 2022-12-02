068172cf29c2523dc29e1e4ccc9d674e3a2341fe
Rkron91 <robin.kron@lerniastudent.se>
2022-12-02 17:47:57 +0100

    daniel @ onemancarry
    Co-authored-by: dansaa8 <dansaa8@users.noreply.github.com>

--- API/Challenge.js
+++ API/Challenge.js
@@ -16,57 +16,7 @@ export class Challenge {
         
         let rating = document.createElement("ul") // alvars funktion
         rating.className = "rating";    
-        //tillfällig
 
-
-        
-        // for(let i = 0; i < 5; i++){
-
-        //   let star = document.createElement("li");
-        //   star.className = "rating-star active";
-        //   rating.appendChild(star);
-        // }
-        //tillfälli
-    setStars();
-
-    function setStars() {
-    let stars = 5;
-
-    let wholeStars;
-    let halfStar;
-    if (Number.isInteger(this.data.rating)) {
-            wholeStars = this.data.rating;
-            createStar(wholeStars, 'whole')
-
-            stars -= wholeStars;
-            createStar(stars);
-    } 
-    else {
-        Math.floor(this.data.rating); 
-
-        wholeStars = this.data.rating; 
-
-        createStar(wholeStars, 'whole')
-        halfStar = 1; 
-        createStar(wholeStars, 'half')
-
-        stars -= wholeStar - HalfStar; 
-        createStar(stars);
-    }
-    
-    function createStar(count, className){
-        for (let i = 0; i < count; i++) {
-            const star = document.createElement('li');
-            star.classList.add('star', className);
-            rating.appendChild(star);
-        }
-    }
-
-   } 
-
-
-       
-        
         let participants = document.createElement("small");
         participants.className = "challenge-meta";
         participants.innerHTML = `${this.data.minParticipants}-${this.data.maxParticipants} participants`;
@@ -74,6 +24,39 @@ export class Challenge {
         let description = document.createElement("p");
         description.className = "challenge-description";
         description.innerHTML = this.data.description    
+
+
+        let stars = 5;
+        let wholeStars;
+        let halfStar;
+    
+        if (Number.isInteger(this.data.rating)) {
+          wholeStars = this.data.rating;
+          createStar(wholeStars, 'fa-solid', 'fa-star');
+    
+          stars -= wholeStars;
+          createStar(stars, 'fa-regular', 'fa-star');
+        } else {
+          const roundedRating = Math.floor(this.data.rating);
+    
+          wholeStars = roundedRating;
+    
+          createStar(wholeStars, 'fa-solid', 'fa-star');
+          halfStar = 1;
+          createStar(halfStar, 'fa-solid', 'fa-star-half-stroke');
+    
+          stars -= roundedRating + halfStar;
+          createStar(stars, 'fa-regular', 'fa-star');
+        }
+        //<i class="fa-solid fa-star"></i>
+        function createStar(count, class1, class2) {
+          for (let i = 0; i < count; i++) {
+            const star = document.createElement("li");
+            star.classList.add(class1, class2);
+            rating.appendChild(star);
+          }
+        }
+    
 
         let KNAPP = document.createElement("button");
         KNAPP.className = "button primary";
@@ -87,26 +70,10 @@ export class Challenge {
         item.appendChild(participants);
         item.appendChild(description);
         item.appendChild(KNAPP);
+    
         return item;
     }
-}
-
-
-
-
-
-/* function addCard(challengeArray) { 
     
-    challengeArray.forEach(ele => {
-        
-        let dom =`<li class="challenge-item">
-        <img class="challenge-image" src="${ele.image}" alt="">
-        <h3 class="challenge-title">${ele.title}</h3>
-        <ul class="rating">${ele.rating}</ul>
-        <small class="challenge-meta">${ele.participants}</small>
-                    <p class="challenge-description">${ele.description}</p>
-                    <button id="${ele.id}" class="button primary">Book this room</button>
-                  </li>`
-        ul.appendChild(dom);
-    })
-} */
+
+
+}
